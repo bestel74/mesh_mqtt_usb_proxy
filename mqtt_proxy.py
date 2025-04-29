40dd21bc5ec03957fec47b65e7f6898bff88b78c
Steven Valsesia <steven.valsesia@gmail.com>
2025-04-29 11:35:43 +0200

    Better reconnection to MQTT if connection lost

--- mqtt_proxy.py
+++ mqtt_proxy.py
@@ -7,12 +7,27 @@ from pubsub import pub
 import paho.mqtt.client as mqtt_client
 
 MQTT_PORT = 1883
-DEV_PATH = "COM16"
+DEV_PATH = "/dev/ttyACM0"
 
 iface = None
+mqtt = None
+mqtt_cfg = None
+
 
 def onReceive(proxymessage, interface):
+    global mqtt
+
     print(f'{proxymessage}', flush=True)
+
+    if not mqtt.is_connected():
+        print("MQTT disconnected, trying to reconnect...", flush=True)
+        try:
+            mqtt_connect()
+            print("Reconnected!", flush=True)
+        except Exception as e:
+            print(f"Reconnect failed: {e}", flush=True)
+            return
+
     it = mqtt.publish(proxymessage.topic, proxymessage.data)
     print(f'{it}', flush=True)
     it.wait_for_publish()
@@ -22,18 +37,27 @@ def on_connect(mqttc, obj, flags, reason_code, properties):
         print("Connected to MQTT Broker!", flush=True)
     else:
         print("Failed to connect, return code %d\n", rc, flush=True)
-        
-def on_reconnect():
-    mqtt_client.connect(mqtt_cfg.address, MQTT_PORT, 10)
-        
+
+def mqtt_connect():
+    global mqtt, mqtt_cfg
+
+    mqtt = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2)
+    mqtt.client_id = iface.getLongName() + "_mqttproxyv2"
+    mqtt.on_connect = on_connect
+    mqtt.on_disconnect = on_disconnect
+    mqtt.username = mqtt_cfg.username
+    mqtt.password = mqtt_cfg.password
+    mqtt.connect(mqtt_cfg.address, MQTT_PORT, 10)
+    mqtt.loop_start()
+
 def on_disconnect(mqttc, obj, flags, reason_code, properties):
-    mqtt_reconnect()
+    mqtt_connect()
 
 
-def get_mqtt_config(dev_path=DEV_PATH):
-    global iface
-    iface = SerialInterface(devPath=dev_path)
+def get_mqtt_config(dp=DEV_PATH):
+    global iface, mqtt_cfg
 
+    iface = SerialInterface(devPath=dp)
     node = iface.localNode
 
     if not node.waitForConfig("moduleConfig"):
@@ -50,21 +74,12 @@ def get_mqtt_config(dev_path=DEV_PATH):
     print(f"  Root: {mqtt_cfg.root}")
     print(f"  TLS Enabled: {mqtt_cfg.tls_enabled}", flush=True)
 
-    global mqtt
-    mqtt = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2)
-    mqtt.client_id = iface.getLongName() + "_mqttproxyv1"
-    mqtt.on_connect = on_connect
-    mqtt.on_disconnect = on_disconnect
-    mqtt.username = mqtt_cfg.username
-    mqtt.password = mqtt_cfg.password
-    mqtt.connect(mqtt_cfg.address, MQTT_PORT, 10)
-    mqtt.loop_start()
-    
-    pub.subscribe(onReceive, 'meshtastic.mqttclientproxymessage')
-
 
 if __name__ == "__main__":
     get_mqtt_config()
+    mqtt_connect()
+
+    pub.subscribe(onReceive, 'meshtastic.mqttclientproxymessage')
 
     try:
         while True:
